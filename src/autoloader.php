af0c8855044fecb9e1ef62b4e7b072faf800ef0e
Tinram <Tinram@users.noreply.github.com>
2022-01-05 11:30:27 +0000

    move PHP prototype into own branch

--- src/autoloader.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-spl_autoload_register(function(string $sClassName): void
-{
-    $sClassName = str_replace('\\', DIRECTORY_SEPARATOR, $sClassName);
-    require $sClassName . '.php';
-});
