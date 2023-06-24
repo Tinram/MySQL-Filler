9f3f3e9577c8ed1c2c74b47cf3aad23468550240
Tinram <Tinram@users.noreply.github.com>
2023-06-24 17:58:59 +0100

    update configuration

--- config.py
+++ config.py
@@ -15,16 +15,18 @@ PROCS = 1                                  # number of processes to spawn
 JUMBLE_FKS = True                          # toggle random jumbling of foreign keys for joins
 FK_PCT_REPLACE = 25                        # percentage of NUM_ROWS of foreign keys to jumble
 
-STRICT_INSERT = False                      # toggle INSERT IGNOREs for duplicate hits (warnings rather than fails)
+STRICT_INSERT = False                      # toggle INSERT IGNOREs for duplicate hits / bypass strict SQL mode (warnings versus errors)
 
-PROCESS_INT_FKS = True                     # default: True;  False for TPCC with tinyints etc in primary keys
-COMPOSITE_PK_INCREMENT = False             # default: False; True for TPCC example of composite primary keys
+PROCESS_INT_FKS = True                     # default: True; process (True) or skip (False) integer foreign keys (TPCC schema with tinyint PKs)
+COMPOSITE_PK_INCREMENT = False             # default: False; skip (False) or increment (True) composite primary keys (TPCC schema)
+
+COMPLEX_JSON = False                       # False for simple fixed JSON data; True to generate variable JSON data
 
 BYTES_DECODE = 'utf-8'                     # character set used for byte data type conversion
-MAX_PACKET = False                         # when root user, maximise the packet size
+MAX_PACKET = False                         # True maximises the packet size (root user only)
 
-DEBUG = False                              # debug toggle
-EXTENDED_DEBUG = False                     # further debug toggle
+DEBUG = False                              # debug output toggle
+EXTENDED_DEBUG = False                     # verbose debug output toggle
 
 TRUNCATE_TABLES = False                    # toggle truncation of all database tables
 
